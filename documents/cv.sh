f413d83194b3a488aa62999c281e5004678b7881
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2023-07-05 09:03:13 -0400

    Deployed  with MkDocs version: 1.4.2

--- documents/cv.sh
+++
@@ -1,3 +0,0 @@
-python cv.py > cv_full.txt
-pandoc --pdf-engine=xelatex cv_full.txt -o cv_full.pdf
-
--- update_page.sh
+++
@@ -1,9 +0,0 @@
-cd docs/documents/
-sh cv.sh
-cd ../..
-mkdocs build
-cp -R site/* .
-rm -Rf site
-git add .
-git commit -m "update"
-git push
