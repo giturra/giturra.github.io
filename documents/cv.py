7a991398b826ef6b59cb8499f9a5af31840058ea
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2023-07-11 13:20:43 -0400

    update

--- documents/cv.py
+++ documents/cv.py
@@ -1,13 +1,8 @@
 
 cv_file="../cv.md"
-pub_file="../publications.md"
 cv_md = open(cv_file, "r", encoding='utf-8')
-pub_md = open(pub_file, "r", encoding='utf-8')
 
 for cv_line in cv_md.readlines():
-    if(cv_line.find("## Publications")>-1):    
-        print(pub_md.read())
-    else:
-        if(cv_line.find("[Full list here]")==-1): 
-            print(cv_line)
+    if(cv_line.find("[Full list here]")==-1): 
+        print(cv_line)
        
